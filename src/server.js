dacc8a46c33f8f45635bffa2c79885ae58ac35f5
RomanFS76 <korvy85@gmail.com>
2026-03-09 12:44:08 +0300

    add express server with pino logger, cors and dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,34 @@
-const message = 'Hello world';
+// src/server.js
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import 'dotenv/config';
 
-console.log(message);
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello RR!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
